7cbb0672d32a9423d1a7be0edb7f863f91dcccbe
senine <senine16@gmail.com>
2014-09-18 10:00:13 -0400

    Complete
    Complete the template function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCatheMatrix provides a structure to cathe and manupulate the assigned matrix
+## and its inverse.
+## cacheSolve caculates the inverse function (if NULL) or reads inverse from
+## makeCatheMatrix.
 
-## Write a short comment describing this function
+## CAUTION: no input check for matrix, assuming inputs are all square and inversable matrices 
+
+## example:
+# x <- matrix(1:4, nrow = 2)
+# c <- makeCacheMatrix(x)
+# cacheSolve(c)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    
+    set <- function (y) {
+        x <<- y
+        inverse <<- NULL # clear the cached inverse since new value of matrix is assigned
+    }
+    get <- function() x
+    set.inv <- function(inv) inverse <<- inv # set inverse manually (unrecommended)
+    get.inv <- function() inverse
+    
+    list(set = set, get = get,
+         set.inv = set.inv,
+         get.inv = get.inv)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$get.inv()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$set.inv(inverse)
+    inverse
 }
+
+
+
+
